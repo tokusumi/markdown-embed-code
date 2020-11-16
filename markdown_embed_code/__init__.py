e17b53fdd9b1c17c03c8b4656f676e197e013965
tokusumi <tksmtoms@gmail.com>
2020-11-17 03:13:37 +0900

    Extend language acceptance

--- markdown_embed_code/__init__.py
+++ markdown_embed_code/__init__.py
@@ -5,8 +5,8 @@ from marko.md_renderer import MarkdownRenderer
 class MarkdownEmbEodeRenderer(MarkdownRenderer):
     def render_fenced_code(self, element):
         lang = element.__dict__.get("lang")
-        lang, *file = lang.split(":", 1)
-        if lang in {"python", "py"} and len(file) == 1:
+        lang, *file = lang.rsplit(":", 1)
+        if len(file) == 1:
             with open(file[0], "r") as f:
                 code = f.read()
             element.children[0].children = f"{code}\n"
