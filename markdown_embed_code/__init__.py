d4c40a13ace4fd1dc9ef2fd06a6d3f0d17f435dd
tokusumi <tksmtoms@gmail.com>
2020-11-16 21:53:09 +0900

    Add main app

---
+++ markdown_embed_code/__init__.py
@@ -0,0 +1,18 @@
+from marko import Markdown
+from marko.md_renderer import MarkdownRenderer
+
+
+class MarkdownEmbEodeRenderer(MarkdownRenderer):
+    def render_fenced_code(self, element):
+        lang = element.__dict__.get("lang")
+        lang, *file = lang.split(":", 1)
+        if lang in {"python", "py"} and len(file) == 1:
+            with open(file[0], "r") as f:
+                code = f.read()
+            element.children[0].children = f"{code}\n"
+        return super().render_fenced_code(element)
+
+
+def get_code_emb():
+    markdown = Markdown(renderer=MarkdownEmbEodeRenderer)
+    return markdown
