f4464a6feb398c0e904a1fd36c2eddf841742cd6
tokusumi <41147016+tokusumi@users.noreply.github.com>
2021-03-26 23:18:52 +0900

    Integrate auto commit action (#6)

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -1,16 +1,63 @@
+from typing import Optional, Sized
+import sys
 from pathlib import Path
+import subprocess
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, SecretStr, BaseModel
+from github import Github
 
 from markdown_embed_code import get_code_emb
 
 
 class Settings(BaseSettings):
     input_markdown: Path = Path("README.md")
+    input_message: str = "📝 Update Readme"
+    input_no_change: str = "No changes on README!"
     input_output: Path = Path("")
+    input_silent: bool = False
+    input_token: SecretStr
+    github_actor: str
+    github_repository: str
+    github_event_path: Path
+
+
+class PartialGitHubEventInputs(BaseModel):
+    number: int
+
+
+class PartialGitHubEvent(BaseModel):
+    number: Optional[int] = None
+    inputs: Optional[PartialGitHubEventInputs] = None
 
 
 settings = Settings()
+subprocess.run(["git", "config", "--local", "user.name", "github-actions"], check=True)
+subprocess.run(
+    ["git", "config", "--local", "user.email", "github-actions@github.com"], check=True
+)
+
+
+g = Github(settings.input_token.get_secret_value())
+repo = g.get_repo(settings.github_repository)
+if not settings.github_event_path.is_file():
+    sys.exit(1)
+contents = settings.github_event_path.read_text()
+event = PartialGitHubEvent.parse_raw(contents)
+if event.number is not None:
+    number = event.number
+elif event.inputs and event.inputs.number:
+    number = event.inputs.number
+else:
+    sys.exit(1)
+pr = repo.get_pull(number)
+if pr.merged:
+    # ignore at merged
+    sys.exit(0)
+
+if not settings.input_output.is_dir():
+    output_path = settings.input_output
+else:
+    output_path = settings.input_markdown
 
 with open(settings.input_markdown, "r") as f:
     doc = f.read()
@@ -18,10 +65,23 @@ with open(settings.input_markdown, "r") as f:
 md = get_code_emb()
 embedded_doc = md(doc)
 
-if not settings.input_output.is_dir():
-    output_path = settings.input_output
-else:
-    output_path = settings.input_markdown
-
 with open(output_path, "w") as f:
     f.write(embedded_doc)
+
+
+proc = subprocess.run(
+    ["git", "status", "--porcelain"], check=True, stdout=subprocess.PIPE
+)
+if not proc.stdout:
+    # no change
+    if not settings.input_silent:
+        pr.create_issue_comment(settings.input_no_change)
+    sys.exit(0)
+
+subprocess.run(["git", "add", output_path], check=True)
+subprocess.run(["git", "commit", "-m", settings.input_message], check=True)
+
+remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
+proc = subprocess.run(["git", "push", remote_repo, f"HEAD:{pr.head.ref}"], check=False)
+if proc.returncode != 0:
+    sys.exit(1)
